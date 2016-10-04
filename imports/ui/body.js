c0e9ff3b820ccc1e86237b94a265e5d0f939035d
Cyrille Dessigny <grimskunk@gmail.com>
2016-10-04 17:40:25 -0400

    pixi.js hello world

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,6 +1,8 @@
 import { Template } from 'meteor/templating';
  
 import './body.html';
+
+import PIXI from 'pixi.js';
  
 Template.body.helpers({
 	  players: [
@@ -9,3 +11,33 @@ Template.body.helpers({
 		      { name: 'Neko', color:"#A0A0A0" },
 		    ],
 });
+
+Template.body.onRendered = function () {
+	console.log("Hellotations");
+	var stage = new PIXI.Container();
+	var renderer = PIXI.autoDetectRenderer(500, 500);
+	document.body.appendChild(renderer.view);
+	PIXI.loader
+		.add("coffee", "https://dl.dropboxusercontent.com/u/139992952/coffee.png")
+		.load(setup);
+	var block;
+
+	function setup() {
+		block = new PIXI.Sprite(PIXI.loader.resources.coffee.texture);  
+		block.anchor.x = 0.5;
+		block.anchor.y = 0.61;
+
+		block.position.x = 200;
+		block.position.y = 150;
+		stage.addChild(block);
+		renderer.render(stage);
+		console.log("rendered");
+		theloop();
+	}
+
+	var theloop = function(){
+		requestAnimationFrame(theloop);
+		block.rotation += .03;
+		renderer.render(stage);
+	};
+};
